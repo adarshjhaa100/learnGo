52076b5730ced08df81031823b8f2c38adaed0c8
Adarsh Jha <adarshjhaa100@gmail.com>
2022-10-23 16:19:09 +0530

    Learn about maps

--- callGreetings/callGreetings.go
+++ callGreetings/callGreetings.go
@@ -9,24 +9,31 @@ import (
 
 func handleAndGreet(name string) {
 	message, error:= greetings.GreetMe(name)
-
-	message, error = randomgreetings.GetRandomGreeting(name)
+	fmt.Println(message)
+	// message, error = randomgreetings.GetRandomGreeting(name)
 	
+	listName:= []string {
+		"Pikachu",
+		"Bulbasaur",
+		"Squirtle", //dont forget this last comma
+	}
+
+	messages,error := randomgreetings.GetMultipleRandomGreetings(listName)
+
 	// if error is not nil, something went wrong
 	if(error!=nil){
 		// handle error message
 		log.Fatal(error)
 	}
+	
+	fmt.Println(messages) // can directly print a map to console
 
-	
-	fmt.Println(message)
-	
 }
 
 
 func main() {
 	name1:= "Bulbasaur"
-	name2:= ""
+	// name2:= ""
 	
 	/*
 		Set prefix of logger and by default time will be printed
@@ -36,7 +43,7 @@ func main() {
 
 	// call function from other package
 	handleAndGreet(name1)
-	handleAndGreet(name2)
+	// handleAndGreet(name2)
 
-	fmt.Println("Hello World")
+	// fmt.Println("Hello World")
 }
